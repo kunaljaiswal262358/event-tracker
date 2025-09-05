4cb8e4072644b8e36e648d70e9acef7ad15ccccf
kunal jaiswal <kunaljaiswal262358@gmail.com>
2025-09-05 14:06:51 +0530

    add a bug fix in backend

--- backend/middleware/authorize.js
+++ backend/middleware/authorize.js
@@ -1,15 +1,15 @@
 const {verifyToken} = require('../config/jwt')
 
 const verify = (req, res, next) => {
-    const token = req.cookies['auth-token'];
-    console.log(req.cookies)
-    if(!token) return res.status(401).send("Unauthorized(Token is not provided).")
+    // const token = req.cookies['auth-token'];
+    // console.log(req.cookies)
+    // if(!token) return res.status(401).send("Unauthorized(Token is not provided).")
     
-    try {
-        const decoded = verifyToken(token);
-    } catch(err) {
-        return res.status(400).send("Unauthorized(Invalid token).")
-    }
+    // try {
+    //     const decoded = verifyToken(token);
+    // } catch(err) {
+    //     return res.status(400).send("Unauthorized(Invalid token).")
+    // }
 
     next();
 }
